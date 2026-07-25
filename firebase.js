8afe43ce8acb0e70c1e8e043bd53b1d1ceb3db73
Hos_Hos <109749794+Houssem028@users.noreply.github.com>
2026-07-26 00:17:24 +0100

    Initialize Firebase SDK and Firestore database

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+// إعدادات مشروع ڨعدة
+const firebaseConfig = {
+    apiKey: "AIzaSyC9Una4Qs9WBHmAb9Qyw3mTJAnJCUNdgDM",
+    authDomain: "gaada-9f860.firebaseapp.com",
+    projectId: "gaada-9f860",
+    storageBucket: "gaada-9f860.firebasestorage.app",
+    messagingSenderId: "268184578905",
+    appId: "1:268184578905:web:db1ba3d5e4d9cb73683a48"
+};
+
+// تشغيل Firebase
+const app = initializeApp(firebaseConfig);
+
+// قاعدة البيانات
+const db = getFirestore(app);
+
+export { db };
