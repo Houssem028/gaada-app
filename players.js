10885e1c3791617b7fb5656ce40e31327eb2d79c
Hos_Hos <109749794+Houssem028@users.noreply.github.com>
2026-07-26 10:55:37 +0100

    Remove players and refactor randomPlayer function
    Removed several player entries from the players array and updated the randomPlayer function to improve clarity.

--- players.js
+++ players.js
@@ -66,44 +66,6 @@ image:"https://upload.wikimedia.org/wikipedia/commons/1/1c/Karim_Benzema_2021.jp
 },
 
 {
-name:"Sadio Mane",
-image:"https://upload.wikimedia.org/wikipedia/commons/9/9d/Sadio_Mane_2022.jpg"
-},
-
-{
-name:"Son Heung-min",
-image:"https://upload.wikimedia.org/wikipedia/commons/1/16/Son_Heung-min_2022.jpg"
-},
-
-{
-name:"Antoine Griezmann",
-image:"https://upload.wikimedia.org/wikipedia/commons/3/3f/Antoine_Griezmann_2021.jpg"
-},
-
-{
-name:"Rodri",
-image:"https://upload.wikimedia.org/wikipedia/commons/9/9d/Rodri_2023.jpg"
-},
-
-{
-name:"Pedri",
-image:"https://upload.wikimedia.org/wikipedia/commons/8/8e/Pedri_2022.jpg"
-},
-
-{
-name:"Gavi",
-image:"https://upload.wikimedia.org/wikipedia/commons/5/55/Gavi_2022.jpg"
-},
-
-{
-name:"Bukayo Saka",
-image:"https://upload.wikimedia.org/wikipedia/commons/8/8c/Bukayo_Saka_2023.jpg"
-}
-
-];
-,
-
-{
 name:"Pele",
 image:"https://upload.wikimedia.org/wikipedia/commons/5/5e/Pele_1970.jpg"
 },
@@ -159,18 +121,8 @@ image:"https://upload.wikimedia.org/wikipedia/commons/8/8d/Gianluigi_Buffon_2017
 },
 
 {
-name:"Paolo Maldini",
-image:"https://upload.wikimedia.org/wikipedia/commons/4/4c/Paolo_Maldini_2008.jpg"
-},
-
-{
 name:"Sergio Ramos",
 image:"https://upload.wikimedia.org/wikipedia/commons/1/1d/Sergio_Ramos_2021.jpg"
-},
-
-{
-name:"Carles Puyol",
-image:"https://upload.wikimedia.org/wikipedia/commons/5/5e/Carles_Puyol_2013.jpg"
 },
 
 {
@@ -184,26 +136,6 @@ image:"https://upload.wikimedia.org/wikipedia/commons/1/1e/David_Beckham_2014.jp
 },
 
 {
-name:"Francesco Totti",
-image:"https://upload.wikimedia.org/wikipedia/commons/2/2e/Francesco_Totti_2014.jpg"
-},
-
-{
-name:"Luis Figo",
-image:"https://upload.wikimedia.org/wikipedia/commons/3/35/Luis_Figo_2017.jpg"
-},
-
-{
-name:"Miroslav Klose",
-image:"https://upload.wikimedia.org/wikipedia/commons/9/9e/Miroslav_Klose_2012.jpg"
-},
-
-{
-name:"Didier Drogba",
-image:"https://upload.wikimedia.org/wikipedia/
-  ,
-
-{
 name:"Zlatan Ibrahimovic",
 image:"https://upload.wikimedia.org/wikipedia/commons/b/b8/Zlatan_Ibrahimovic_2019.jpg"
 },
@@ -214,76 +146,6 @@ image:"https://upload.wikimedia.org/wikipedia/commons/1/15/Luis_Suarez_2018.jpg"
 },
 
 {
-name:"Edinson Cavani",
-image:"https://upload.wikimedia.org/wikipedia/commons/7/7e/Edinson_Cavani_2018.jpg"
-},
-
-{
-name:"Arjen Robben",
-image:"https://upload.wikimedia.org/wikipedia/commons/1/16/Arjen_Robben_2014.jpg"
-},
-
-{
-name:"Franck Ribery",
-image:"https://upload.wikimedia.org/wikipedia/commons/8/82/Franck_Ribery_2019.jpg"
-},
-
-{
-name:"Mesut Ozil",
-image:"https://upload.wikimedia.org/wikipedia/commons/1/18/Mesut_Ozil_2018.jpg"
-},
-
-{
-name:"Bastian Schweinsteiger",
-image:"https://upload.wikimedia.org/wikipedia/commons/7/7c/Bastian_Schweinsteiger_2018.jpg"
-},
-
-{
-name:"Manuel Neuer",
-image:"https://upload.wikimedia.org/wikipedia/commons/9/92/Manuel_Neuer_2018.jpg"
-},
-
-{
-name:"Toni Kroos",
-image:"https://upload.wikimedia.org/wikipedia/commons/1/1b/Toni_Kroos_2019.jpg"
-},
-
-{
-name:"Thomas Muller",
-image:"https://upload.wikimedia.org/wikipedia/commons/3/3e/Thomas_Muller_2019.jpg"
-},
-
-{
-name:"Philipp Lahm",
-image:"https://upload.wikimedia.org/wikipedia/commons/1/1e/Philipp_Lahm_2014.jpg"
-},
-
-{
-name:"Wayne Rooney",
-image:"https://upload.wikimedia.org/wikipedia/commons/7/7d/Wayne_Rooney_2016.jpg"
-},
-
-{
-name:"Steven Gerrard",
-image:"https://upload.wikimedia.org/wikipedia/commons/8/87/Steven_Gerrard_2014.jpg"
-},
-
-{
-name:"Frank Lampard",
-image:"https://upload.wikimedia.org/wikipedia/commons/5/55/Frank_Lampard_2014.jpg"
-},
-
-{
-name:"John Terry",
-image:"https://upload.wikimedia.org/wikipedia/commons/9/9c/John_Terry_2015.jpg"
-},
-
-{
-name:"Didier Deschamps",
-image:"https://upload.wikimedia.org/wikipedia/commons/8/8e/Didier_Deschamps_2018.jpg"
-},
-
-{
 name:"Riyad Mahrez",
 image:"https://upload.wikimedia.org/wikipedia/commons/8/87/Riyad_Mahrez_2021.jpg"
 },
@@ -291,19 +153,10 @@ image:"https://upload.wikimedia.org/wikipedia/commons/8/87/Riyad_Mahrez_2021.jpg
 {
 name:"Achraf Hakimi",
 image:"https://upload.wikimedia.org/wikipedia/commons/5/5f/Achraf_Hakimi_2022.jpg"
-},
-
-{
-name:"Victor Osimhen",
-image:"https://upload.wikimedia.org/wikipedia/commons/3/3d/Victor_Osimhen_2023.jpg"
-},
-
-{
-name:"Lautaro Martinez",
-image:"https://upload.wikimedia.org/wikipedia/commons/1/1c/Lautaro_Martinez_2022.jpg"
 }
 
 ];
+
 
 
 // اختيار لاعب عشوائي بدون تكرار
@@ -311,46 +164,25 @@ image:"https://upload.wikimedia.org/wikipedia/commons/1/1c/Lautaro_Martinez_2022
 export function randomPlayer(usedPlayers = []){
 
 
-    let available = players.filter(
-        player => !usedPlayers.includes(player.name)
-    );
-
-
-    if(available.length === 0){
-
-        available = players;
-
-    }
-
-
-    return available[
-        Math.floor(Math.random() * available.length)
-    ];
-
-}
-  // اختيار لاعب عشوائي بدون تكرار
-
-export function randomPlayer(usedPlayers = []) {
-
-    // اللاعبين الذين لم يتم استخدامهم
     let availablePlayers = players.filter(
+
         player => !usedPlayers.includes(player.name)
+
     );
 
 
-    // إذا انتهت القائمة نعيد السماح بكل اللاعبين
-    if (availablePlayers.length === 0) {
+
+    if(availablePlayers.length === 0){
 
         availablePlayers = players;
 
     }
 
 
-    // اختيار عشوائي
-    const random =
-    Math.floor(Math.random() * availablePlayers.length);
 
+    return availablePlayers[
+        Math.floor(Math.random() * availablePlayers.length)
+    ];
 
-    return availablePlayers[random];
 
 }
